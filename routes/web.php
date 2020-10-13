38442845f0eccd3be0fe50e3b4ed2d7823f51f17
marcela <marcelaguerrero1396@gmail.com>
2020-10-13 17:12:50 -0500

    Completando Ejercicio Video Club Parte 1

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,32 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// Ruta /
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+// Ruta Login
+Route::get('login', function () {
+    return view('auth.login');
+});
+// Ruta logout
+Route::get('logout', function () {
+    return "Logout usuario";
+});
+// Ruta Catalogo
+Route::get('catalog', function () {
+    return view('catalog.index');
+});
+// Ruta Catalogo/Show => Parametros
+Route::get('catalog/show/{id}', function ($id) {
+    return view('catalog.show', array('id' => $id));
+});
+// Ruta Catalogo/Create
+Route::get('catalog/create', function () {
+    return view('catalog.create');
+});
+// Ruta Catalogo/Edit => Parametros
+Route::get('catalog/edit/{id}', function ($id) {
+    return view('catalog.edit', array('id' => $id));
+});
+
